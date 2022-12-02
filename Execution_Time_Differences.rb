f50f91097b55922d60468fa7a4e652b4d23b8761
Jiong Qi <jiongqipan@gmail.com>
2022-12-02 13:56:06 -0500

    continuous subsum

--- Execution_Time_Differences.rb
+++ Execution_Time_Differences.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 def my_min(list)        # 0(n^2)
     list.each do |el|
        return el if list.all? {|el2| el <= el2}
@@ -15,3 +17,59 @@ end
 # p    list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p    my_min_2(list)  # =>  -5
 
+def largest_contiguous_subsum(list) # O(n^2)
+    # sums = []
+    # (0...list.length).each do |idx1|
+    #     (0...list.length).each do |idx2|
+    #         sums << list[idx1..idx2].sum if idx1 <= idx2
+    #     end
+    # end
+    # sums.max
+
+    arr = []
+    i = 0
+    while i < list.length
+        j = i
+        while j < list.length
+            arr << list[i..j]
+            j += 1
+        end
+        i += 1
+    end
+    sum_arr = arr.map {|ele| ele.sum}
+    sum_arr.max
+end
+
+list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
+
+list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+# p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+def largest_contiguous_subsum_2(list)
+    return list.max if list.all? {|ele| ele < 0}
+    max = - 1 / 1.0
+    a, b = 0, 0
+    while a < list.length && b < list.length
+        if list[a..b] + list[b] > max
+            max = list[a] + list[b]
+            b += 1
+        else
+            a += 1
+            b += 1
+        end
+    end
+    return max
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum_2(list) # => 8
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum_2(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum_2(list) # => -1 (from [-1])
